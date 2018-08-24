95f9ccc6fafcdfd62c2fc72a1bee0dc510bd33e5
Antonio Rodriguez <arodriguez@velocitypartners.net>
2018-08-24 09:27:31 -0300

    initial commit

---
+++ KatanaSample/KatanaSample/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Course https://app.pluralsight.com/player?course=aspdotnet-mvc5-fundamentals
+namespace KatanaSample
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8086";
+
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Started!");
+                Console.ReadKey();
+                Console.WriteLine("Stopping!");
+            }
+
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use<HelloWorldComponent>();
+            //app.Run(ctx => {
+            //    return ctx.Response.WriteAsync("Hello world!!");
+            //});
+        }
+    }
+
+    public class HelloWorldComponent
+    {
+        AppFunc _next;
+
+        public HelloWorldComponent(AppFunc next)
+        {
+            this._next = next;
+        }
+
+        public Task Invoke(IDictionary<string, object> environment)
+        {
+            var response = environment["owin.ResponseBody"] as Stream;
+
+            using (var writer = new StreamWriter(response))
+            {
+                return writer.WriteAsync("Hello!!");
+            }
+        }
+    }
+}
